23037b141f21d469a13eb6d37b3cc7af3d6a3ee2
Ryan-Rutledge <ryan.rutledge.dev@gmail.com>
2020-12-22 18:04:07 -0600

    Change DataGenerator to accept a model object
    This allows the DataGenerator to be input-agnostic because it
    is no longer responsible for traversing the dict. The model will
    have to be created separately, but will allow multiple syntaxes to
    be more easily altered and supported at the same time

--- data_generator.py
+++ data_generator.py
@@ -1,37 +1,94 @@
-import itertools
 import random
 import re
+from abc import ABCMeta, abstractmethod
 from majormode.utils.namegen import NameGeneratorFactory
 
 
-class StringGenerator:
-    """Dict value manipulator and fake data generator"""
+class BasicNode(metaclass=ABCMeta):
+    """Abstract node for creating traversable generator models"""
+
+    def __init__(self, conditions=None):
+        self.conditions = conditions or []
+
+    def __iter__(self):
+        while True:
+            yield next(self)
+
+    @abstractmethod
+    def __next__(self):
+        pass
+
+    def meets_conditions(self, data):
+        for condition in self.conditions:
+            if not condition(data):
+                return False
+        return True
+
+
+class WrapperNode(BasicNode):
+    """Applies arbitrary functionality to a contained node"""
+
+    def __init__(self, node=None, conditions=None):
+        super().__init__(conditions)
+        self.node = node
+
+    def __next__(self):
+        return next(self.node)
+
+
+class NamedNode(WrapperNode):
+    """Applies a name to a node"""
+
+    def __init__(self, name, node=None, conditions=None):
+        super().__init__(node, conditions)
+        self.name = name
+
+
+class RepeaterNode(WrapperNode):
+    """Generate data an arbitrary number of times"""
+
+    def __init__(self, node=None, reps=None, conditions=None):
+        super().__init__(node, conditions)
+
+        if reps is None:
+            reps = (1, 1)
+        elif isinstance(reps, int):
+            reps = (reps, reps)
+
+        self.min_reps, self.max_reps = reps
+
+    def __next__(self):
+        reps = random.randint(self.min_reps, self.max_reps)
+        return [next(self.node) for _ in range(reps)]
+
+
+class StringGenerator(BasicNode):
+    """Random string generator"""
 
     _field_regex = re.compile(r'\[(?P<field>\w+?)]')
     _name_regex = re.compile(r'{{2}name:(?P<language>\w+?)(?::(?P<min>\d+):(?P<max>\d+))?}{2}')
 
-    def __init__(self):
-        # Dict of language names and their corresponding generators
-        self.name_generators = dict(zip(
-            [str(language) for language in NameGeneratorFactory.Language],
-            [NameGeneratorFactory.get_instance(language) for language in NameGeneratorFactory.Language]
-        ))
+    _name_generators = dict(zip(
+        [str(language) for language in NameGeneratorFactory.Language],
+        [NameGeneratorFactory.get_instance(language) for language in NameGeneratorFactory.Language]
+    ))
 
-    def sub(self, key, data):
-        """Replace placeholder text in data dict"""
+    def __init__(self, string, conditions=None):
+        super().__init__(conditions)
+        self.model_string = string
 
-        # Generate names
-        data[key] = self._name_regex.sub(self._sub_name, data[key])
+    def __next__(self):
+        data = self.model_string
+        data = self._name_regex.sub(self._sub_name, data)
+        return data
 
-        # Replace field references
-        data[key] = self._field_regex.sub(lambda m: data.get(m.groups()[0]), data[key])
-
-    def _sub_name(self, match):
+    @classmethod
+    def _sub_name(cls, match):
         """Replace name generator language regex match with random name"""
 
         groups = match.groupdict()
         language = groups['language']
-        generator = self.name_generators.get(language)
+        generator = cls._name_generators.get(language)
 
         if generator is not None:
             # Use minimum and maximum syllables if they are provided
@@ -48,104 +105,86 @@ class StringGenerator:
             return name
 
 
-class DataGenerator:
-    """Arbitrary data generator"""
+class SelectionGenerator(BasicNode, metaclass=ABCMeta):
+    """Random list selection node"""
 
-    _conditional_field_regex = re.compile(r'^(?P<not>!)?\[(?P<field>.*)](?:=(?P<value>.*))?$')
-    _repeat_field_regex = re.compile(r'^(?P<field>.+)\*(?:(?P<repetitions>\d+)|(?P<min>\d)+-(?P<max>\d+))$')
-    _hidden_field_regex = re.compile(r'^_.+$')
+    def __init__(self, choices, min_size=1, max_size=1, conditions=None):
+        super().__init__(conditions)
+        self.min_size, self.max_size = min_size, max_size
+        self.choices = choices
 
-    def __init__(self, model):
-        self.data_model = model
-        self.string_generator = StringGenerator()
+    def __next__(self):
+        count = random.randint(self.min_size, self.max_size)
+        selection = self._selection(count)
+        data = list([next(d) for d in selection])
+        return self._list_or_element(data)
+
+    @abstractmethod
+    def _selection(self, count):
+        pass
+
+    def _list_or_element(self, items):
+        return items[0] if self.min_size == 1 and self.max_size == 1 else items
+
+
+class SampleGenerator(SelectionGenerator):
+    """Random elements sampler"""
+
+    def _selection(self, count):
+        return random.sample(self.choices, k=count)
+
+
+class ChoiceGenerator(SelectionGenerator):
+    """Random elements chooser"""
+
+    def __init__(self, choices, weights=None, min_size=1, max_size=1, conditions=None):
+        super().__init__(choices, min_size, max_size, conditions)
+
+        # Default all weights to 1
+        self.weights = weights or [1]*len(self.choices)
+
+    def _selection(self, count):
+        return random.choices(self.choices, weights=self.weights, k=count)
+
+
+class DictGenerator(BasicNode):
+    """Arbitrary dict generator"""
+
+    def __init__(self, nodes=None, conditions=None):
+        super().__init__(conditions)
+        self.nodes = nodes or []
+
+    def __next__(self):
+        data = {}
+
+        for node in self.nodes:
+            if node.meets_conditions(data):
+                data[node.name] = next(node)
+
+        return data
+
+
+class DataGeneratorModel:
+    def __init__(self, root_node):
+        self.root = root_node
 
     def __iter__(self):
         while True:
             yield next(self)
 
+    @abstractmethod
     def __next__(self):
-        return self._generate_object()
+        return next(self.root)
 
-    def _generate_object(self, model=None):
-        """Generate a dict of random data"""
 
-        data_model = self.data_model if model is None else model
-        data = {}
+class DataGenerator:
+    def __init__(self, model):
+        self.model = model
 
-        # Generate random data for each field
-        for field, val in data_model.items():
-            self._generate_field(field, val, data)
+    def __iter__(self):
+        while True:
+            yield next(self)
 
-        self._clean_data(data)
-
-        return data
-
-    def _clean_data(self, data):
-        """Replace placeholder text and applies post-generation cleanup"""
-
-        hidden_fields = []
-        for key in data.keys():
-            # Substitute placeholders
-            if isinstance(data[key], str):
-                self.string_generator.sub(key, data)
-
-            # Record field to be removed if it's marked as hidden
-            if self._hidden_field_regex.match(key):
-                hidden_fields.append(key)
-
-        # Remove hidden fields
-        for hidden_field in hidden_fields:
-            del data[hidden_field]
-
-    def _generate_field(self, key, model, data):
-        """Generate model data and adds it to data[key]"""
-
-        repeating_field_match = self._repeat_field_regex.match(key)
-
-        if repeating_field_match:
-            # Generate a self-contained list of random data
-            groups = repeating_field_match.groupdict()
-            field, reps = groups['field'], groups['repetitions']
-            min_reps, max_reps = reps or groups['min'], reps or groups['max']
-            reps = random.randint(int(min_reps), int(max_reps))
-
-            data[field] = [self._generate_object(model) for _ in range(reps)]
-        else:
-            # Recursively generate a single instance of data
-            if isinstance(model, list):
-                self._generate_from_list(key, model, data)
-            elif isinstance(model, dict):
-                self._generate_from_dict(key, model, data)
-            elif model is not None:
-                data[key] = model
-
-    def _generate_from_list(self, key, model, data):
-        """Randomly select a list item"""
-
-        is_weighted = len(model) > 1 and\
-            model[0] is not None and\
-            len(model[0]) == 2 and\
-            isinstance(model[0][1], (int, float))
-
-        if is_weighted:
-            population, weights = itertools.zip_longest(*model, fillvalue=1)
-            random_value = random.choices(population, weights, k=1)
-        else:
-            random_value = random.choice(model)
-
-        self._generate_field(key, random_value, data)
-
-    def _generate_from_dict(self, key, model, data):
-        """Extract fields from dictionary if condition is met"""
-
-        conditional_field_match = self._conditional_field_regex.match(key)
-        if conditional_field_match is not None:
-            groups = conditional_field_match.groupdict()
-            reverse_condition = bool(groups.get('not'))
-            reference_field = data.get(groups.get('field'))
-            value = groups.get('value')
-            condition_met = reference_field is not None and (value is None or value == reference_field)
-
-            if reverse_condition ^ condition_met:
-                for inner_key, inner_val in model.items():
-                    self._generate_field(inner_key, inner_val, data)
+    @abstractmethod
+    def __next__(self):
+        return next(self.model)
